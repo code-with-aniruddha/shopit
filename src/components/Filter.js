0c4e570825f62f9d7edce52045ec18ffac498fb4
ani <aniruddha.khan2011@gmail.com>
2022-06-11 20:20:59 +0100

    added cart component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <Navbar expand="lg" bg="secondary" variant="dark">
+      <Container>
+        {/* <Navbar.Brand href="#home">Filter</Navbar.Brand> */}
+        <Nav className="me-auto">
+          {/* <Nav.Link href="#features">Features</Nav.Link> */}
+          <Form.Check type="checkbox" label="exclude out of stock" />
+          <Form.Check type="checkbox" label="fast delivery only" />
+        </Nav>
+        <Nav>
+          <Button variant="light">clear filter</Button>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Filter;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { FaShoppingCart } from "react-icons/fa";
+import { Badge } from "react-bootstrap";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Shop IT</Navbar.Brand>
-        <Navbar.Text>text</Navbar.Text>
+        <Navbar.Text>
+          <Button variant="info">
+            <FaShoppingCart fontSize="25px"></FaShoppingCart>
+            10
+          </Button>
+        </Navbar.Text>
       </Container>
     </Navbar>
   );
