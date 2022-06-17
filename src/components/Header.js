123f5ea080361dc179972882b2de6d4bc9d4ef05
ani <aniruddha.khan2011@gmail.com>
2022-06-17 16:56:58 +0100

    added routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,22 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { FaShoppingCart } from "react-icons/fa";
-import { Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Shop IT</Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>Shop IT</Navbar.Brand>
+        </Link>
         <Navbar.Text>
-          <Button variant="info">
-            <FaShoppingCart fontSize="25px"></FaShoppingCart>
-            10
-          </Button>
+          <Link to="/cart">
+            <Button variant="info">
+              <FaShoppingCart fontSize="25px"></FaShoppingCart>
+              10
+            </Button>
+          </Link>
         </Navbar.Text>
       </Container>
     </Navbar>
