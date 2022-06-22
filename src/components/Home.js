fe4443e54324a702de604f7ce9f0c7eb0011986a
ani <aniruddha.khan2011@gmail.com>
2022-06-22 14:55:36 +0100

    moved filter on left

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,26 @@ import { ProductContext } from "../context/ProductContext";
 import Product from "./Product";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Filter from "./Filter";
 
 const Home = () => {
   const {
     state: { products },
   } = useContext(ProductContext);
   return (
-    <Row className="g-4">
-      {products.map((product) => (
-        <Col xs={12} sm={6} md={4} key={product.id}>
-          <Product product={product} />
-        </Col>
-      ))}
+    <Row>
+      <Col className="filter" md={3}>
+        <Filter />
+      </Col>
+      <Col md={9}>
+        <Row className="g-4">
+          {products.map((product) => (
+            <Col xs={12} sm={6} md={4} key={product.id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Col>
     </Row>
   );
 };
